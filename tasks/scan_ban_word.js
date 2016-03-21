cc585ed230ea411b2b0e4f39919eb9837223c459
nonjene <reanfn89@gmail.com>
2016-03-21 18:15:35 +0800

    bug fix

--- tasks/scan_ban_word.js
+++ tasks/scan_ban_word.js
@@ -11,7 +11,9 @@ module.exports = function ( grunt ) {
         var opt = this.options();
         var filesIgnore = [ 'node_modules/**', '.*/**', '.*', 'Gruntfile.js' ];
 
-        var flagError = util.scanWord( this.filesSrc, filesIgnore,  opt.banList);
+
+
+        var flagError = util.scanWord( grunt.file.expand( { filter: 'isFile' }, this.filesSrc ), filesIgnore,  opt.banList);
         if( flagError){
             throw new Error( 'Scan ban word task failed! ' );
         }
